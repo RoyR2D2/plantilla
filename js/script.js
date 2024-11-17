0e767df6476116bdd088d67b47bcb393e26b9b98
RoyR2D2 <roybustillobolea@gmail.com>
2024-11-17 19:32:22 +0100

    Version 17/11/2024

--- js/script.js
+++ js/script.js
@@ -6,6 +6,25 @@ function updatemenu() {
 	  document.getElementById('menu').style.borderRadius = '0px';
 	}
   }
+  let slideIndex = 0;
+  showSlides();
   
+  function changeSlide(n) {
+	showSlides(slideIndex += n);
+  }
+  
+  function showSlides() {
+	let i;
+	let slides = document.getElementsByClassName("slide");
+	for (i = 0; i < slides.length; i++) {
+	  slides[i].style.display   
+   = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {slideIndex = 1}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlides,   
+   4000); // Cambia la imagen cada 4 segundos
+  }
   
   
